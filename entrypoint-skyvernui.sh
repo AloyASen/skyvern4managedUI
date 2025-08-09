34ca4378c8e44e9fd9e5c7005ef1663a2ce98c1e
Aloy Aditya Sen <aloysen951@gmail.com>
2025-08-09 21:57:47 +0530

    [Frontend] Fix: derive host machine fingerprint

--- entrypoint-skyvernui.sh
+++ entrypoint-skyvernui.sh
@@ -5,6 +5,9 @@ set -e
 # setting api key
 VITE_SKYVERN_API_KEY=$(sed -n 's/.*cred\s*=\s*"\([^"]*\)".*/\1/p' .streamlit/secrets.toml)
 export VITE_SKYVERN_API_KEY
+# compute system fingerprint for license validation
+VITE_SYSTEM_FINGERPRINT=$(python -c 'from skyvern.utils.fingerprint import SYSTEM_FINGERPRINT; print(SYSTEM_FINGERPRINT)')
+export VITE_SYSTEM_FINGERPRINT
 npm run start
 
 
